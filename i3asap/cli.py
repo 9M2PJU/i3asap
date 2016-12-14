118c30bdeff6b25c4de06a27e6d30bb792c67b8e
Steve Tabernacle <stevetabernacle@users.noreply.github.com>
2016-12-14 03:06:25 +0100

    logging

--- i3asap/cli.py
+++ i3asap/cli.py
@@ -8,6 +8,7 @@ from datetime import datetime
 
 from helpers import slugify, fetchJSON
 from models import AsynkDownloader, DebianLinux
+import logging
 
 
 @click.command()
@@ -25,23 +26,29 @@ def main(bundle, ok):
     """
     startTime = datetime.now()
 
+    # check if it is reasonable to start at all
     if ok != 'yes':
         sys.exit()
-
     linux = DebianLinux()
     if not linux.verifyOS():
         click.echo("Currently this program only supports Kali Linux. Exiting..")
         sys.exit()
 
+    # Setup properties
+    repository = "https://raw.githubusercontent.com/SteveTabernacle/i3asap/"
     bundle = slugify(bundle)
     pwd = linux.home_dir() + "/.i3asap/" + bundle + "/"
-    repository = "https://raw.githubusercontent.com/SteveTabernacle/i3asap/"
 
     if not os.path.exists(pwd):
         os.makedirs(pwd)
 
-    # Download manifest to get started
-    click.echo("Downloading " + bundle)
+    # Init logging
+    LOG_FILENAME = pwd+'debug.log'
+    logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
+    logging.debug("Started new instance "+str(startTime))
+
+    # Download manifest
+    click.echo("* Downloading " + bundle)
 
     manifest = fetchJSON(repository + "master/bundles/" + bundle + "/manifest.json")
 
@@ -49,25 +56,29 @@ def main(bundle, ok):
     downloads = AsynkDownloader(manifest["files"], pwd, repository + "master/bundles/" + bundle + "/bundle/")
     downloads.start()
 
+    click.echo("* Uninstalling " + manifest["uninstall"])
     # Purge specified programs
     if "purge" in manifest and len(manifest["uninstall"]) > 1:
-        linux.uninstall(manifest["uninstall"])
+        logging.debug(linux.uninstall(manifest["uninstall"]))
 
+    click.echo("* Installing " + manifest["install"])
     # Install specified programs
     if "install" in manifest and len(manifest["install"]) > 1:
-        linux.install(manifest["install"])
+        logging.debug(linux.install(manifest["install"]))
 
     # Install i3
+    click.echo("* Installing " + linux.i3_base_packages())
     linux.install(linux.i3_base_packages())
 
     # todo Create new user
     # Wait until downloads are complete
     downloads.join()
+    click.echo("* Download finished ")
 
     # todo Move dotfiles, wallpapers etc to proper paths
     # todo Set wallpaper if specified
 
-    click.echo("Done! Time elapsed: " + str(datetime.now() - startTime))
+    click.echo("* Done! Time elapsed: " + str(datetime.now() - startTime))
     # todo Switch user to new user
 
 
