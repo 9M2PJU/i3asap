1f6f0e322699658e16d54cbc5b5f2c513857a2f3
Steve Tabernacle <stevetabernacle@users.noreply.github.com>
2016-12-14 03:42:22 +0100

    fix

--- i3asap/cli.py
+++ i3asap/cli.py
@@ -54,8 +54,8 @@ def main(bundle, ok):
 
     # Download all specified files, e.g. dotfiles and wallpaper
 
-    wallpaper = AsynkDownloader({"name": manifest["wallpaper"],
-                                 "saveAs": "/usr/share/backgrounds/" + manifest["wallpaper"]}, repository)
+    wallpaper = AsynkDownloader([{"name": manifest["wallpaper"],
+                                  "saveAs": "/usr/share/backgrounds/" + manifest["wallpaper"]}], repository)
 
     dotfiles = AsynkDownloader(manifest["dotfiles"], repository)
     dotfiles.start()
